e8d8f6a3309ffcdef80b24fe2f8bc6eb186ad815
Madeleine Johnson <mjohnson3038@gmail.com>
2016-11-28 21:19:53 -0800

    Display list of stock using only QuoteView NOT ApplicationView

--- src/app.js
+++ src/app.js
@@ -5,20 +5,20 @@ import Backbone from 'backbone';
 // import TaskView from 'app/views/quote_view.js';
 // import ApplicationView from 'app/views/application_view.js';
 
-// const simulate = function(quote) {
-//   // Calculate a random price movement
-//   const maxChange = 1.00;
-//   const minChange = 0.00;
-//   var change = _.random(minChange * 10, maxChange * 10) / 10;
-//
-//   // Decide if the change is positive or negative
-//   if(_.random(0,1) === 1) {
-//     change *= -1;
-//   }
-//
-//   // Actually trigger the change
-//   quote.trigger('change:price', change);
-// };
+const simulate = function(quote) {
+  // Calculate a random price movement
+  const maxChange = 1.00;
+  const minChange = 0.00;
+  var change = _.random(minChange * 10, maxChange * 10) / 10;
+
+  // Decide if the change is positive or negative
+  if(_.random(0,1) === 1) {
+    change *= -1;
+  }
+
+  // Actually trigger the change
+  quote.trigger('change:price', change);
+};
 
 var stockData = [
   {
